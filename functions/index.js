c7366c7ef282ed23d24687afbf334fcd42c3ce7e
leoyeh <leo@iprefer.com.tw>
2017-10-24 10:02:47 +0800

    [u] text-indent

--- functions/index.js
+++ functions/index.js
@@ -11,9 +11,9 @@ const cors = require('cors')
 const rp = require('request-promise')
 
 function helloFn2(req, res) {
- const json = JSON.parse(req.body)
- const idToken = json.idToken
- admin.auth().verifyIdToken(idToken)
+  const json = JSON.parse(req.body)
+  const idToken = json.idToken
+  admin.auth().verifyIdToken(idToken)
  .then((decodedToken) => {
    // console.log('decode ', decodedToken)
    var uid = decodedToken.uid
@@ -32,20 +32,20 @@ function helloFn2(req, res) {
 }
 
 function helloFn(req, res) {
- res.status(200).send({ uid: 12344444 })
+  res.status(200).send({ uid: 12344444 })
 }
 
 function show(msg, res) {
- res.status(200).send(msg)
+  res.status(200).send(msg)
 }
 
 exports.verify = functions.https
 .onRequest((req, res) => {
  // res.status(200).send(rep)
- var corsFn = cors()
- corsFn(req, res, () => {
+  var corsFn = cors()
+  corsFn(req, res, () => {
    // helloFn(req, res)
-   helloFn2(req, res)
+    helloFn2(req, res)
    // show(req.body, res)
    // const json = JSON.parse(req.body)
    // console.log('json ', json.idToken)
@@ -53,38 +53,38 @@ exports.verify = functions.https
    // rep.result = true
    // rep.token = json.idToken
    // res.status(200).send(rep)
- })
+  })
 })
 
 function corsShow(req, res, msg) {
- const corsFn = cors()
- corsFn(req, res, () => {
-   const rep = {}
-   rep.result = true
-   rep.msg = msg
-   res.send(rep)
- })
+  const corsFn = cors()
+  corsFn(req, res, () => {
+    const rep = {}
+    rep.result = true
+    rep.msg = msg
+    res.send(rep)
+  })
 }
 
 exports.checkRecaptcha = functions.https.onRequest((req, res) => {
- const json = JSON.parse(req.body)
- const token = json.token
+  const json = JSON.parse(req.body)
+  const token = json.token
  // console.log('recaptcha response', rep.token)
- rp({
-   uri: 'https://recaptcha.google.com/recaptcha/api/siteverify',
-   method: 'POST',
-   formData: {
-     secret: '6LcZBDUUAAAAAA2D1beYQmQGQ6TJMKu4idPz3RvT',
-     response: token,
-   },
-   json: true,
- }).then((result) => {
-   if (result.success) {
-     corsShow(req, res, "You're good to go, human.")
-   } else {
-     corsShow(req, res, 'Recaptcha verification failed. Are you a robot?')
-   }
- }).catch((reason) => {
-   corsShow(req, res, 'Recaptcha request failed.')
- })
+  rp({
+    uri: 'https://recaptcha.google.com/recaptcha/api/siteverify',
+    method: 'POST',
+    formData: {
+      secret: '6LcZBDUUAAAAAA2D1beYQmQGQ6TJMKu4idPz3RvT',
+      response: token,
+    },
+    json: true,
+  }).then((result) => {
+    if (result.success) {
+      corsShow(req, res, "You're good to go, human.")
+    } else {
+      corsShow(req, res, 'Recaptcha verification failed. Are you a robot?')
+    }
+  }).catch((reason) => {
+    corsShow(req, res, 'Recaptcha request failed.')
+  })
 })
